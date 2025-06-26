f1ed2de3cf2bb594d4074584e591e42cf7a5951e
İrem Beril <irmberil03@gmail.com>
2025-06-26 09:33:08 +0300

    calculator arayüz geliştirildi ve double-digit pipe eklendi

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -17,26 +17,58 @@ export class CalculatorComponent {
   historyVisible = false;
   history: string[] = [];
 
+  // Tuş takımı değerlerini ayrı tanımladık
+  keys: string[] = [
+    'C',
+    '()',
+    '/',
+    '←',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    'G',
+    '0',
+    '.',
+    '=',
+  ];
+
   constructor(
     private calcService: CalculatorService,
     private historyService: HistoryService
   ) {}
 
+  // Tuşa basıldığında yapılacak işlemler
   press(key: string): void {
-    if (key === '=') {
-      this.result = this.expression;
-      this.expression = this.calcService.evaluate(this.expression);
-    } else if (key === 'C') {
-      this.expression = '';
-    } else if (key == '←') {
-      this.expression = this.expression.slice(0, -1);
-    } else if (key == 'G') {
-      this.toggleHistory();
-    } else {
-      this.expression += key;
+    switch (key) {
+      case '=':
+        this.result = this.expression;
+        this.expression = this.calcService.evaluate(this.expression);
+        break;
+      case 'C':
+        this.expression = '';
+        break;
+      case '←':
+        this.expression = this.expression.slice(0, -1);
+        break;
+      case 'G':
+        this.toggleHistory();
+        break;
+      default:
+        this.expression += key;
+        break;
     }
   }
 
+  // Geçmiş görünürlüğünü aç/kapa yapar
   toggleHistory(): void {
     this.historyVisible = !this.historyVisible;
     if (this.historyVisible) {
